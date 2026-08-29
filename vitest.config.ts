99ea1866653a71c455afc51ea6c582a0cc72d7e8
Illia Brashkin <214269542+illia-m-b@users.noreply.github.com>
2026-08-29 12:03:18 +0300

    chore: configure `vitest` (#16)

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026 Illia Brashkin
+ * SPDX-License-Identifier: MIT
+ */
+
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    include: ['test/**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      include: ['src/**/*.ts'],
+      thresholds: {
+        100: true,
+      },
+    },
+  },
+});
