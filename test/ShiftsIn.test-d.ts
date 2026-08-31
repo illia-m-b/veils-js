18a41b978a25b61caf2b679cca787c14ed60932a
Illia Brashkin <214269542+illia-m-b@users.noreply.github.com>
2026-08-31 16:38:49 +0300

    feat: add `ShiftsIn` type definition and static tests (#27)

--- test/ShiftsIn.test-d.ts
+++ test/ShiftsIn.test-d.ts
@@ -2,3 +2,21 @@
  * SPDX-FileCopyrightText: Copyright (c) 2026 Illia Brashkin
  * SPDX-License-Identifier: MIT
  */
+
+import { expectTypeOf, test } from 'vitest';
+
+import { ShiftsIn } from '../src/ShiftsIn.js';
+
+test('infers input shifts correctly', (): void => {
+  interface Page {
+    id: string;
+    load: () => string;
+    save(content: ArrayBuffer): string;
+  }
+  interface PageShiftsIn {
+    id?: never;
+    load?: () => [];
+    save?(content: ArrayBuffer): [ArrayBuffer];
+  }
+  expectTypeOf<ShiftsIn<Page>>().toEqualTypeOf<PageShiftsIn>();
+});
