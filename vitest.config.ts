f94fd9f33184ebd0830c517ea356aac0580a43cb
Illia Brashkin <214269542+illia-m-b@users.noreply.github.com>
2026-09-04 23:43:02 +0300

    chore: optimize test performance and enforce strict linting (#84)

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,19 @@ export default defineConfig({
   test: {
     environment: 'node',
     include: ['test/**/*.test.ts'],
+    typecheck: {
+      enabled: true,
+    },
+    pool: 'threads',
+    isolate: false,
+    sequence: {
+      shuffle: true,
+    },
+    slowTestThreshold: 100,
+    testTimeout: 2000,
+    onConsoleLog(): never {
+      throw new Error('Logs are forbidden in test suites');
+    },
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
